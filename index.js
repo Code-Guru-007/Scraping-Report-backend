407ebedc151610c8fa7163126d3afc431008ee79
code-guru-007 <py.guru.007@gmail.com>
2025-02-06 17:21:29 +0900

    db connected

--- index.js
+++ index.js
@@ -6,36 +6,32 @@ const cors = require('cors');
 const server = express();
 const path = require('path');
 const productRouter = require('./routes/product')
-const userRouter = require('./routes/user')
 // console.log('env',process.env.DB_PASSWORD)
 
 //db connection
-main().catch(err => console.log(err));
+main();
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
-  console.log('database connected')
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/scrapereport');
+    console.log('database connected')
+  } catch (error) {
+    console.log(error)
+  }
 }
 //Schema
-
-
-
-
-
-
 
 
 //bodyParser
 server.use(cors());
 server.use(express.json());
 server.use(morgan('default'));
-server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
+// server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
 server.use('/products',productRouter.router);
-server.use('/users',userRouter.router);
-server.use('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'build','index.html'))
-})
 
+// server.use('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'build','index.html'))
+// })
 
 
 
